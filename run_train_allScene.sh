a6208f85fceaf3a118e099c299ae890a8afb4c84
apple-guobk <baokui2011@163.COM>
2020-12-08 17:29:18 +0800

    multiGPU

--- run_train_allScene.sh
+++ run_train_allScene.sh
@@ -81,3 +81,21 @@ nohup python -u run_pretraining.py --input_file=$files  \
 --train_batch_size=64 --max_seq_length=$max_seq_length --max_predictions_per_seq=2 \
 --num_train_steps=200000 --num_warmup_steps=10000 --learning_rate=1e-4  --tpu_name=123  \
 --save_checkpoints_steps=3000 >> log/pretrain-bert_prose48.log 2>&1 &
+
+BERT_BASE_DIR=model/bert_prose_finetune
+my_new_model_path=$BERT_BASE_DIR/ckpt
+mkdir -p $my_new_model_path
+max_seq_length=48
+export CUDA_VISIBLE_DEVICES=3
+filelist=`ls data_allScene_pretrain/tfrecord48/`
+array=($filelist)
+files=data_allScene_pretrain/tfrecord48/${array[0]}
+for((i=1;i<${#array[@]};i++))
+do
+  files=$files,data_allScene_pretrain/tfrecord48/${array[i]}
+done
+nohup python -u run_pretraining.py --input_file=$files  \
+--output_dir=$my_new_model_path --do_train=True --do_eval=True --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+--train_batch_size=64 --max_seq_length=$max_seq_length --max_predictions_per_seq=2 \
+--num_train_steps=2000000 --num_warmup_steps=10000 --learning_rate=1e-4  --tpu_name=123  \
+--save_checkpoints_steps=3000 --init_checkpoint=model/bert_prose/ckpt/model.ckpt-200000 >> log/pretrain-bert_prose_finetune48.log 2>&1 &
