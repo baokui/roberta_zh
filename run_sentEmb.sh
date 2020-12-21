658749a018c1f4ee9e492a5261394e52bbbddf1e
apple-guobk <baokui2011@163.COM>
2020-12-21 16:28:38 +0800

    model-s2v

--- run_sentEmb.sh
+++ run_sentEmb.sh
@@ -125,3 +125,27 @@ vocab_file='model/bert_prose_finetune_mgpu/vocab.txt'
 path_idf='data_allScene_pretrain/IDF.json'
 max_seqlen=48
 nohup python -u sentEmb.py $gpu $path_data $path_target $init_checkpoint $bert_config_file $vocab_file $max_seqlen $tag $path_idf >> log/sent-$tag-$File.log 2>&1 &
+
+File=Queries1
+gpu=0
+tag=allScenePre48-weightedmean-0
+path_data="/search/odin/guobk/vpa/vpa-studio-research/retrieval/data/$File.json"
+path_target="/search/odin/guobk/vpa/vpa-studio-research/retrieval/data/$File-$tag.json"
+init_checkpoint='model/bert_allScene48/ckpt/model.ckpt'
+bert_config_file='model/bert_allScene48/bert_config.json'
+vocab_file='model/bert_allScene48_0/vocab.txt'
+path_idf='data_allScene_pretrain/IDF.json'
+max_seqlen=48
+nohup python -u sentEmb.py $gpu $path_data $path_target $init_checkpoint $bert_config_file $vocab_file $max_seqlen $tag $path_idf >> log/sent-$tag-$File.log 2>&1 &
+
+File=Docs
+gpu=0
+tag=allScenePre48-weightedmean-0
+path_data="/search/odin/guobk/vpa/vpa-studio-research/retrieval/data/$File.json"
+path_target="/search/odin/guobk/vpa/vpa-studio-research/retrieval/data/$File-$tag.json"
+init_checkpoint='model/bert_allScene48/ckpt/model.ckpt'
+bert_config_file='model/bert_allScene48/bert_config.json'
+vocab_file='model/bert_allScene48_0/vocab.txt'
+path_idf='data_allScene_pretrain/IDF.json'
+max_seqlen=48
+nohup python -u sentEmb.py $gpu $path_data $path_target $init_checkpoint $bert_config_file $vocab_file $max_seqlen $tag $path_idf >> log/sent-$tag-$File.log 2>&1 &
