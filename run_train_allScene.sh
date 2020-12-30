b8f9c280462e7eac6345bc02e66cca89e86d5087
apple-guobk <baokui2011@163.COM>
2020-12-30 20:13:31 +0800

    bert-flow

--- run_train_allScene.sh
+++ run_train_allScene.sh
@@ -199,4 +199,24 @@ nohup python -u run_pretraining_mGPU.py --input_file=$files  --n_gpus=4 \
 --output_dir=$my_new_model_path --do_train=True --do_eval=True --bert_config_file=$BERT_BASE_DIR/bert_config.json \
 --train_batch_size=64 --max_seq_length=$max_seq_length --max_predictions_per_seq=2 \
 --num_train_steps=2000000 --num_warmup_steps=10000 --learning_rate=1e-4  --tpu_name=123  \
---save_checkpoints_steps=3000 --init_checkpoint=>> log/pretrain-bert_allScene48.log 2>&1 &
+--save_checkpoints_steps=3000 --init_checkpoint=$init_checkpoint >> log/pretrain-bert_allScene48-finetune.log 2>&1 &
+
+
+BERT_BASE_DIR=model/bert_allScene48
+my_new_model_path=$BERT_BASE_DIR/ckpt_flow
+init_checkpoint=$BERT_BASE_DIR/ckpt/model.ckpt
+mkdir -p $my_new_model_path
+max_seq_length=48
+export CUDA_VISIBLE_DEVICES="6,7"
+filelist=`ls data_allScene_pretrain/tfrecord48/`
+array=($filelist)
+files=data_allScene_pretrain/tfrecord48/${array[0]}
+for((i=1;i<${#array[@]};i++))
+do
+  files=$files,data_allScene_pretrain/tfrecord48/${array[i]}
+done
+nohup python -u run_pretraining_mGPU_flow.py --input_file=$files  --n_gpus=2 \
+--output_dir=$my_new_model_path --do_train=True --do_eval=True --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+--train_batch_size=64 --max_seq_length=$max_seq_length --max_predictions_per_seq=2 \
+--num_train_steps=2000000 --num_warmup_steps=10000 --learning_rate=1e-4  --tpu_name=123  \
+--save_checkpoints_steps=3000 --init_checkpoint=$init_checkpoint >> log/pretrain-bert_allScene48-flow.log 2>&1 &
