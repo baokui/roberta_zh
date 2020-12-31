068d71ee146f66f27f6bc8fc806f6c06559b6fcc
apple-guobk <baokui2011@163.COM>
2020-12-31 15:14:41 +0800

    bert-flow

--- run_pretraining_mGPU_flow.py
+++ run_pretraining_mGPU_flow.py
@@ -791,9 +791,11 @@ def main(_):
           masked_lm_loss,
           aggregation_method=tf.AggregationMethod.EXPERIMENTAL_TREE,
       )
+      average_grads = average_gradients([grads], None, None)
+      average_grads, norm_summary_ops = clip_grads(average_grads, 10.0, True)
       #(grads, _) = tf.clip_by_global_norm(grads, 10.0,True)
-      grads, norm_summary_ops = clip_grads(grads, 10.0, True)
-      train_op = optimizer.apply_gradients(grads, global_step=global_step)
+      #grads, norm_summary_ops = clip_grads(grads, 10.0, True)
+      train_op = optimizer.apply_gradients(average_grads, global_step=global_step)
       # train_op = optimizer.apply_gradients(
       #     zip(grads, tvars), global_step=global_step)
 
